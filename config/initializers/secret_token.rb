faf03674e9d07c08f143418e95d3fe06423485c2
Chris Lovell <chrisl1991@hotmail.co.uk>
2013-11-24 21:32:53 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TypeOff::Application.config.secret_key_base = 'e9eceb146e37f74f3b2e356dee9659136b93d73a9ad2aae5c96813882ac22add5aa3e37787d3bb5e702465263af3083c7e375d2ff259b5d2f5ad7f72129dfc40'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TypeOff::Application.config.session_store :cookie_store, key: '_TypeOff_session'

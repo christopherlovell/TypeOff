648d26c84e3b31ec04c9e42d1146ab9eb6316dc2
Polyphant1 <chrisl1991@hotmail.co.uk>
2013-11-26 22:29:08 +0000

    updated html files. Includes CSS, JS & Images

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,6 +7,11 @@ describe "StaticPages" do
       visit '/static_pages/home'
       expect(page).to have_content('TypeOff')
     end
+    
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("TypeOff | Home")
+    end
   end
   
   describe "Help page" do
@@ -14,12 +19,22 @@ describe "StaticPages" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+    
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title("TypeOff | Help")
+    end
   end
     
   describe "Leaderboard page" do
     it "Should have the content 'Leaderboard' " do
       visit '/static_pages/leaderboard'
       expect(page).to have_content('Leaderboard')
+    end
+    
+    it "should have the right title" do
+      visit '/static_pages/leaderboard'
+      expect(page).to have_title("TypeOff | Leaderboard")
     end
   end
       

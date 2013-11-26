dd3841dad6c06dc49de373943f81f9d115f5684e
Polyphant1 <chrisl1991@hotmail.co.uk>
2013-11-26 16:59:33 +0000

    Updated gemfile with rspec

--- Gemfile
+++ Gemfile
@@ -27,14 +27,23 @@ gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
 
+group :development , :test do
+  gem 'rspec-rails', '~> 2.13.1'
+end
+
+group :test do
+  gem 'selenium-webdriver', '~> 2.35.1'
+  gem 'capybara', '~> 2.1.0'
+end
+  
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
   gem 'sdoc', require: false
 end
 
 group :production do
-  gem 'pg', '0.15.1'
-  gem 'rails_12factor', '0.0.2'
+  gem 'pg', '~> 0.15.1'
+  gem 'rails_12factor', '~> 0.0.2'
 end
 
 # Use ActiveModel has_secure_password
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,4 +1,4 @@
-# Be sure to restart your server when you modify this file.
+	# Be sure to restart your server when you modify this file.
 
 # Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-TypeOff::Application.config.secret_key_base = 'e9eceb146e37f74f3b2e356dee9659136b93d73a9ad2aae5c96813882ac22add5aa3e37787d3bb5e702465263af3083c7e375d2ff259b5d2f5ad7f72129dfc40'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token

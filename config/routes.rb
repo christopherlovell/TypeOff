d5341f9f01280f8afbedd0a54fbc4121fd1f7c25
Polyphant1 <chrisl1991@hotmail.co.uk>
2013-12-14 14:23:25 +0000

    new changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 TypeOff::Application.routes.draw do
   resources :fonts
   
-  root "home#index"
+  root to: "home#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
